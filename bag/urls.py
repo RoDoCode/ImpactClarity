06c1b9e6abe137fdc66f4f7594e03a7f8c0ace96
Rory Dow <rory.dow@hotmail.com>
2024-05-04 23:44:17 +0000

    update urls.py in bag app to allow for adding series and tokens to bag

--- bag/urls.py
+++ bag/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
     path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
     path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
+    path('add/<item_id>/', views.add_series_to_bag, name='add_series_to_bag'),
+    path('adjust/<item_id>/', views.adjust_series_in_bag, name='adjust_series_in_bag'),
+    path('remove/<item_id>/', views.remove_series_from_bag, name='remove_series_from_bag'),
+    path('add/<item_id>/', views.add_token_to_bag, name='add_token_to_bag'),
+    path('adjust/<item_id>/', views.adjust_token_in_bag, name='adjust_token_in_bag'),
+    path('remove/<item_id>/', views.remove_token_from_bag, name='remove_token_from_bag'),
 ]
