e05accf7a85ef3aff6a87c8978167fe9dec42111
Rory Dow <rory.dow@hotmail.com>
2024-05-08 16:56:16 +0000

    Add access key field for products on user profile model

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from products.models import Product, Series, CoachingToken
 
 from django_countries.fields import CountryField
 
@@ -12,20 +13,17 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20,
-                                            null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80,
-                                               null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80,
-                                               null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40,
-                                            null=True, blank=True)
-    default_county = models.CharField(max_length=80,
-                                      null=True, blank=True)
-    default_postcode = models.CharField(max_length=20,
-                                        null=True, blank=True)
-    default_country = CountryField(blank_label='Country',
-                                   null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
+    series_access = models.ManyToManyField(Series, related_name='series_user', blank=True,)
+    product_access = models.ManyToManyField(Product, related_name='product_user', blank=True,)
+    token_access = models.ManyToManyField(CoachingToken, related_name='token_user', blank=True,)
+
 
     def __str__(self):
         return self.user.username
