eb8e3f23086ad633b5ff74f8cf69d536ee8d7901
Rory Dow <rory.dow@hotmail.com>
2024-05-13 01:21:25 +0000

    Hide series access and product access in profile page

--- profiles/forms.py
+++ profiles/forms.py
@@ -5,7 +5,7 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user',)
+        exclude = ('user', 'product_access', 'series_access')
 
     def __init__(self, *args, **kwargs):
         """
